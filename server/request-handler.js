bb733a3ca590114c8b1a2c1ca758ea71bee8f441
jayvilla <villarina.jeffrey@gmail.com>
2017-01-10 15:28:30 -0800

    Pass all Mocha tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,86 +11,54 @@ var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
-
-var send404Request = function(response) {
-  response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: Page not found!');
-  response.end();
-};
-
-var send201Request = function(response) {
-  response.writeHead(201, {'Content-Type': 'text/plain'});
-  response.write('201 Request: The request has been fulfilled and has resulted in one or more new resources being created.');
-  response.end();
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'text/plain'
 };
 
 var results = {results: []};
 
-
-
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
+  // headers['Content-Type'] = 'text/plain';
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
+  console.log(results);
+
   if (request.method == 'GET' && request.url == '/classes/messages') {
     console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
-    // The outgoing status.
     var statusCode = 200;
 
-    // See the note below about CORS headers.
     var headers = defaultCorsHeaders;
 
-    // Tell the client we are sending them plain text.
-    //
-    // You will need to change this if you are sending something
-    // other than plain text, like JSON or HTML.
-    headers['Content-Type'] = 'text/plain';
-
-    // .writeHead() writes to the request line and headers of the response,
-    // which includes the status and all headers.
     response.writeHead(statusCode, headers);
     console.log('response-status code: ', response.statusCode);
 
-    // Make sure to always call response.end() - Node may not send
-    // anything back to the client until you do. The string you pass to
-    // response.end() will be the body of the response - i.e. what shows
-    // up in the browser.
-    //
-    // Calling .end "flushes" the response's internal buffer, forcing
-    // node to actually send all the data over to the client.
     var stringifiedResponse = JSON.stringify(results);
     response.end(stringifiedResponse);
     
     } else if (request.method == 'POST' && request.url == '/classes/messages') {
       console.log('Serving request type ' + request.method + ' for url ' + request.url);
-      send201Request(response);
+      response.writeHead(201, defaultCorsHeaders);
+      request.on('data', function(data) {
+        var parsedData = JSON.parse(data);
+        results.results.push(parsedData);
+      });
+      // response.write('201 Request: The request has been fulfilled and has resulted in one or more new resources being created.');
+      response.end();
     } else {
-      send404Request(response);
+      response.writeHead(404, defaultCorsHeaders);
+      // response.write('Error 404: Page not found!');
+      response.end();
     }
 };
 
-// // These headers will allow Cross-Origin Resource Sharing (CORS).
-// // This code allows this server to talk to websites that
-// // are on different domains, for instance, your chat client.
-// //
-// // Your chat client is running from a url like file://your/chat/client/index.html,
-// // which is considered a different domain.
-// //
-// // Another way to get around this restriction is to serve you chat
-// // client from this domain by setting up static file serving.
+// These headers will allow Cross-Origin Resource Sharing (CORS).
+// This code allows this server to talk to websites that
+// are on different domains, for instance, your chat client.
+//
+// Your chat client is running from a url like file://your/chat/client/index.html,
+// which is considered a different domain.
+//
+// Another way to get around this restriction is to serve you chat
+// client from this domain by setting up static file serving.
 
 module.exports.requestHandler = requestHandler;
